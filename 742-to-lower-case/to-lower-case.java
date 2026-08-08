1acda071d323e422d102838729650c4942d43944
Aaditya Siddharth Bansod <aadityasbansod@gmail.com>
2026-08-08 23:49:14 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (67.81%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,17 +1,6 @@
 class Solution {
     public String toLowerCase(String s) {
 
-        StringBuilder ans = new StringBuilder();
-        for(int i = 0 ; i<s.length() ; i++){
-            if((int)s.charAt(i)<=90 && (int)s.charAt(i)>64){
-                ans.append((char)(s.charAt(i)+32));
-            }
-            else{
-                ans.append(s.charAt(i));
-            }
-        }
-
-        return ans.toString();
-        
+        return s.toLowerCase();
     }
 }
