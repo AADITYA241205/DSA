00506f0fc5c4c510aeac00d82b5282703bc2f347
Aaditya Siddharth Bansod <aadityasbansod@gmail.com>
2026-08-11 11:13:54 +0530

    Time: 6 ms (99.69%) | Memory: 47.9 MB (59.90%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -24,10 +24,11 @@ class Solution {
     public boolean check(int[] piles , int mid , int h){
         long sum = 0;
         for(int i = 0 ; i<piles.length ; i++){
-            sum+= piles[i]/mid;
-            if(piles[i]%mid!=0){
-                sum+=1;
-            }
+            // sum+= piles[i]/mid;
+            // if(piles[i]%mid!=0){
+            //     sum+=1;
+            // }
+            sum += (piles[i]+mid-1)/mid;
         }
         if(sum<=h){
             return true;
