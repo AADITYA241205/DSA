c24b9c276704aa5c97e181777684e96676d90e46
Aaditya Siddharth Bansod <aadityasbansod@gmail.com>
2026-09-08 02:46:15 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (10.85%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,26 @@
+class Solution {
+    
+    public double myPow(double x, int n) {
+        int i = 0;
+        long nn = n;
+        double ans = 1;
+
+        if(nn<0)nn = (-1)*nn;
+
+        while(nn>0){
+            if(nn%2==0){
+                x = x*x;
+                nn=nn/2;
+            }
+            else{
+                ans = ans *x;
+                nn-=1;
+            }
+        }
+        if(n<0) return (double)(1.0/(double)ans);
+
+        return ans;
+
+    }
+
+}
